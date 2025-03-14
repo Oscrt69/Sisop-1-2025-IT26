0446ea430adc00d34d5ea26d8f5867fe7349e9e6
Oscarrrrrrrrrrrrrrrr <oscaryavat3@gmail.com>
2025-03-14 15:57:21 +0700

    Add files via upload

--- soal_1/poppo_shiroyo.sh
+++ soal_1/poppo_shiroyo.sh
@@ -1,1 +1,58 @@
+echo "Pilih tugas yang ingin dijalankan:"
+echo "1. Menghitung jumlah buku yang dibaca oleh Chris Hemsworth"
+echo "2. Menghitung rata-rata durasi membaca dengan Tablet"
+echo "3. Mencari pembaca dengan rating tertinggi"
+echo "4. Mencari genre paling populer setelah 31 Desember 2023"
+read -p "Masukkan Pilihan" input
 
+if [ "$input" == "1" ] 
+then
+   sumbook=$(awk -F',' '$2 == "Chris Hemsworth" {count++} END {print count}' reading_data.csv)
+   echo "Chris Hemsworth membaca $sumbook buku."
+
+elif [ "$input" == "2" ] 
+then 
+     declare -A durasi perangkat
+     while IFS=',' read -r _ _ _ _ _ duration _  device _; 
+          do
+           durasi["Tablet"]=$((${durasi["Tablet"]} + duration))
+           perangkat["Tablet"]=$((${perangkat["Tablet"]} + 1))
+     done < reading_data.csv 
+
+     if [ -n "${durasi[Tablet]}" ]
+     then
+        avg=$(echo "scale=2; ${durasi[Tablet]} / ${perangkat[Tablet]}" | bc)
+        echo "Rata-rata durasi membaca dengan Tablet adalah $avg menit."
+fi
+
+elif [ "$input" == "3" ]
+    then
+     best=$(awk -F',' 'NR>1 {if ($7 > max) {max=$7; name=$2; book=$3; rating=$7}} END {print name " - " book " - " rating}' reading_data.csv)
+     echo "Pembaca dengan rating tertinggi: $best"
+
+elif [ "$input" == "4" ]
+then
+   declare -A genre_count
+   while IFS=',' read -r _  _  _ genre _ date  _ _ _; 
+   do
+    if [[ "$date" > "2023-12-31" ]] 
+    then
+        genre_count[$genre]=$((${genre_count[$genre]:-0} + 1))
+    fi
+done < reading_data.csv
+
+max_genre=""
+num=0
+for i  in "${!genre_count[@]}"; do
+    if (( genre_count[$i] > num ))
+    then
+        num=${genre_count[$i]}
+        max_genre=$i
+    fi
+done
+
+    echo "Genre paling populer setelah 2023 adalah $max_genre dengan $num buku."
+
+else
+    echo "Pilihan tidak valdi"
+fi 
