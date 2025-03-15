17038222981c3cfa9b95f7619ca1c357292dffbf
Oscarrrrrrrrrrrrrrrr <oscaryavat3@gmail.com>
2025-03-15 18:48:11 +0700

    Delete soal_2 directory

--- soal_2/scripts/core_monitor.sh
+++
@@ -1,6 +0,0 @@
-cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
-cpu_model=$(lscpu | grep "Model name" | sed 's/.*: *//')
-echo "CPU Usage: $cpu_usage%"
-echo "CPU Model: $cpu_model"
-timestamp=$(date '+%Y-%m-%d %H:%M:%S')
-echo "[$timestamp] - Core Usage [$cpu_usage%] - Terminal Model [$cpu_model]" >> ./core.log
--- soal_2/scripts/frag_monitor.sh
+++
@@ -1,6 +0,0 @@
-ram_usage=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
-ram_total=$(free -m | grep Mem | awk '{print $2}')
-ram_used=$(free -m | grep Mem | awk '{print $3}')
-ram_available=$(free -m | grep Mem | awk '{print $7}')
-timestamp=$(date '+%Y-%m-%d %H:%M:%S')
-echo "[$timestamp] - Fragment Usage [$ram_usage%] - Fragment Count [$ram_used MB] - Details [Total: $ram_total MB, Available: $ram_available MB]" >> ./logs/fragment.log
