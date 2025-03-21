79eee6face9a43cd6c255ee70458dd2635142f97
Oscarrrrrrrrrrrrrrrr <oscaryavat3@gmail.com>
2025-03-21 10:27:11 +0700

    ganti

--- soal_2/scripts/core_monitor.sh
+++
@@ -1,4 +0,0 @@
-cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
-cpu_model=$(lscpu | grep "Model name" | cut -d ':' -f 2 | xargs)
-timestamp=$(date '+%Y-%m-%d %H:%M:%S')
-echo "[$timestamp] - Core Usage [$cpu_usage%] - Terminal Model [$cpu_model]" >> ./logs/core.log
